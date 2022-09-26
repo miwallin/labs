b57f513ea56c010387c4a6656d5fcfa1cbe16dee
miwa4922 <miriamw84@gmail.com>
2022-09-26 16:56:37 +0200

    somewhat prettier version

--- lab7/script.js
+++ lab7/script.js
@@ -1,3 +1,11 @@
+const width = document.getElementById("width");
+const length = document.getElementById("length");
+const height = document.getElementById("height");
+
+width.addEventListener("input", (event) => {
+    
+});
+
 function cuboid (width, length, height) {
     return width*length*height;
 };
