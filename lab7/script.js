ca91b972d663b7fff18dd38778227c1c7b32c6f6
miwa4922 <miriamw84@gmail.com>
2022-09-27 12:36:26 +0200

    adding some functionality

--- lab7/script.js
+++ lab7/script.js
@@ -1,11 +1,37 @@
-const width = document.getElementById("width");
-const length = document.getElementById("length");
-const height = document.getElementById("height");
+// Paragraf
+const uppdatera = document.querySelector("#uppdatera");
+uppdatera.addEventListener('click', () => document.querySelector("#paragraf p").textContent = "Denna paragraf är ny och fräsch.");
 
-width.addEventListener("input", (event) => {
-    
-});
+// Länk
+const linkChanger = document.querySelector("#linkbutton");
+linkChanger.addEventListener('click', () => {
+    const linkElement = document.querySelector("#linkchange a");
+    linkElement.setAttribute("href", "https://google.com");
+    linkElement.textContent = "Google";
+    linkChanger.setAttribute("class", "d-none");
+  });
 
+// Kub
 function cuboid (width, length, height) {
     return width*length*height;
 };
+
+const inputs = document.querySelectorAll(".kubdata");
+
+inputs.forEach(element => element.addEventListener("input", (event) => {
+    let width = document.querySelector("#width").value;
+    let length = document.querySelector("#length").value;
+    let height = document.querySelector("#height").value;
+    if (width && length && height) {
+        let result = cuboid(width, length, height);
+        if (width == length && length == height) {
+            document.querySelector("#resultat").textContent = "Din kub har volymen: " + result;
+        }
+        else {
+            document.querySelector("#resultat").textContent = "Ditt rätblock har volymen: " + result;
+        }
+    }
+    else {
+        document.querySelector("#resultat").textContent = "";
+    }
+}));
