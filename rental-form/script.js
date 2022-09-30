2e8a9c46dfbefbd8dd5cb6ecd9b937e60651c270
miwa4922 <miriamw84@gmail.com>
2022-09-30 18:49:05 +0200

    checkbox behaviours + colour changes

--- rental-form/script.js
+++ rental-form/script.js
@@ -1,6 +1,21 @@
 const form = document.querySelector("#hyr");
 form.addEventListener("submit", handleFormSubmit);
 
+const noExtras = document.querySelector('#tb0');
+const extras = document.querySelectorAll('.extra');
+
+noExtras.addEventListener('change', () => {
+    if (noExtras.checked) {
+        extras.forEach(extra => {
+            extra.disabled = true;
+            extra.checked = false;
+        });
+    } 
+    else {
+        extras.forEach(extra => extra.disabled = false);
+    }
+  });
+
 function validEmail(email) {
     let re = /([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
@@ -21,4 +36,15 @@ function handleFormSubmit(event) {
         console.log("Invalid phone number");
     }
     else {console.log("Valid phone number");}
+    if (!noExtras.checked){
+        let additionals = [];
+        extras.forEach(extra => {
+            if (extra.checked) {
+                additionals.push(extra.value);
+            }
+        });
+        if (additionals.length === 0) {
+            console.log("Pick something");
+        }
+    }
 }
