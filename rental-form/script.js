92cf5d9fab199ec19db77657cb9cceb946311c9d
miwa4922 <miriamw84@gmail.com>
2022-10-06 17:29:48 +0200

    reorganizing submitfunction

--- rental-form/script.js
+++ rental-form/script.js
@@ -22,7 +22,7 @@ const startDate = document.querySelector('#startdatum');
 const endDate = document.querySelector('#slutdatum');
 const today = new Date();
 const threeDays = new Date(today.getFullYear(),today.getMonth(),(today.getDate()+4));
-const bookableLimit = new Date(today.getFullYear(),(today.getMonth()+6),today.getDate());
+const bookableLimit = new Date(today.getFullYear(),(today.getMonth()+3),today.getDate());
 
 startDate.setAttribute("value", today.toISOString().slice(0,10));
 startDate.setAttribute("min", today.toISOString().slice(0,10));
@@ -43,45 +43,53 @@ function validPhone(number) {
     return re.test(number);
 }
 
+// Submit
+const emailAdress = document.querySelector("#email");
+const mailfel = document.querySelector("#mailfel");
+const telefon = document.querySelector("#telefon");
+const telfel = document.querySelector("#telfel");
+const tbhBox = document.querySelector('#extras');
+const datumfel = document.querySelector("#datumfel");
+const tbfel = document.querySelector("#tbfel");
+
 function handleFormSubmit(event) {
     event.preventDefault();
     //Email
-    let errormessage = document.querySelector("#mailfel");
-    if (!validEmail(document.querySelector("#email").value)) {
+    let errormessage = mailfel;
+    if (!validEmail(emailAdress.value)) {
         errormessage.textContent = "Ogiltig email-adress";
     }
     else {errormessage.textContent = "";}
     //Telefon
-    errormessage = document.querySelector("#telfel");
-    if (!validPhone(document.querySelector("#telefon").value)) {
+    errormessage = telfel;
+    if (!validPhone(telefon.value)) {
         errormessage.textContent = "Ogiltigt telefonnummer";
     }
     else {errormessage.textContent = "";}
     //Datum
-    errormessage = document.querySelector("#datumfel");
-    if (document.querySelector("#slutdatum").value < document.querySelector("#startdatum").value) {
+    errormessage = datumfel;
+    if (endDate.value < startDate.value) {
         errormessage.textContent = "Hämtdatum får ej vara tidigare än returdatum"
     }
     else {errormessage.textContent = "";}
     //Tillbehör
-    let box = document.querySelector('#extras');
-    errormessage = document.querySelector("#tbfel");
+    errormessage = tbfel;
     if (!noExtras.checked){
         let additionals = [];
         extras.forEach(extra => {
             if (extra.checked) {
                 additionals.push(extra.value);
-                box.removeAttribute("class", "extraserror");
+                tbhBox.removeAttribute("class", "extraserror");
                 errormessage.textContent = "";
             }
         });
         if (additionals.length === 0) {
-            box.setAttribute("class", "extraserror");
+            tbhBox.setAttribute("class", "extraserror");
             errormessage.textContent = "Välj minst ett av alternativen"
         }
     }
     else {
-        box.removeAttribute("class", "extraserror");
+        tbhBox.removeAttribute("class", "extraserror");
         errormessage.textContent = "";
     }
 }
