62b33a714f879f2b371f56fbd9146ed13a3a8094
miwa4922 <miriamw84@gmail.com>
2022-11-04 13:24:57 +0100

    buttons and some styling

--- to-do/public/script.js
+++ to-do/public/script.js
@@ -23,17 +23,26 @@ const buildTodoList = () => {
 const buildTodoDiv = todo => {
   let todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
+
   let title = document.createElement('h4');
   title.classList.add('todo-title');
   title.innerText = todo.task_title;
   let content = document.createElement('p');
   content.classList.add('todo-content');
   content.innerText = todo.task_content;
-  let button = document.createElement('button');
-  button.textContent = 'Done';
+
+  let buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('button-div');
+  let fButton = document.createElement('button');
+  fButton.textContent = 'Finished';
+  let dButton = document.createElement('button');
+  dButton.textContent = 'Delete';
+  buttonDiv.appendChild(fButton);
+  buttonDiv.appendChild(dButton);
+
   todoDiv.appendChild(title);
   todoDiv.appendChild(content);
-  todoDiv.appendChild(button);
+  todoDiv.appendChild(buttonDiv);
 
   return todoDiv;
 }
